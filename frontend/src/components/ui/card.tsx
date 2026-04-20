61d0a6674d5da49cd13cd17b31fb640f299aed0b
nicholas-source <nicholas4585@gmail.com>
2026-04-20 13:39:35 +0100

    Refactor Card and Select components for improved layout and spacing

--- frontend/src/components/ui/card.tsx
+++ frontend/src/components/ui/card.tsx
@@ -9,7 +9,7 @@ Card.displayName = "Card";
 
 const CardHeader = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
   ({ className, ...props }, ref) => (
-    <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />
+    <div ref={ref} className={cn("flex flex-col gap-1.5 p-space-lg", className)} {...props} />
   ),
 );
 CardHeader.displayName = "CardHeader";
@@ -29,13 +29,13 @@ const CardDescription = React.forwardRef<HTMLParagraphElement, React.HTMLAttribu
 CardDescription.displayName = "CardDescription";
 
 const CardContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />,
+  ({ className, ...props }, ref) => <div ref={ref} className={cn("px-space-lg pb-space-lg pt-0", className)} {...props} />,
 );
 CardContent.displayName = "CardContent";
 
 const CardFooter = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
   ({ className, ...props }, ref) => (
-    <div ref={ref} className={cn("flex items-center p-6 pt-0", className)} {...props} />
+    <div ref={ref} className={cn("flex items-center px-space-lg pb-space-lg pt-0", className)} {...props} />
   ),
 );
 CardFooter.displayName = "CardFooter";
